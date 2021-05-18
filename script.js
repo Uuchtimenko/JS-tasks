8e5a55b5ae54eae3a14fef42dc1dffa36addbb61
Dmitry Ustimenko <uuctimenko@gmail.com>
2021-05-18 10:21:33 +0300

    Add comments,fixed '==' to '===',fixed code-style.

--- script.js
+++ script.js
@@ -1,30 +1,36 @@
-//1
-const testArray = [1,3,0,5,7,-8,32,5,32,-16,7,5,3];
+//Дан массив из чисел.Сделайте из него массив, состоящий из квадратов этих чисел.
+const testArray = [1, 3, 0, 5, 7, -8, 32, 5, 32, -16, 7, 5, 3];
 const sqrArray = [];
 testArray.forEach(function(item){
-    sqrArray.push(item*item);
+    sqrArray.push(item * item);
 } )
 console.log (sqrArray);
-//2
-const filterArray = testArray.filter((item,index) => testArray.indexOf(item) == index);
+
+//Дан массив из чисел.Верните массив, состоящий только из уникальных значений(убрать все дубликаты, число в новом массиве не должно повторяться)
+const filterArray = testArray.filter((item, index) => testArray.indexOf(item) === index);
 console.log(filterArray);
-//3
+
+//Дан массив из чисел.Проверьте то, что все элементы в массиве больше нуля.
 console.log(testArray.every(item => item >= 0));
-//4
+//Дан массив из чисел.Оставьте в нем только отрицательные числа
 const negativeArray = testArray.filter(item => item < 0);
 console.log(negativeArray);
-//5
-const sumItems = testArray.reduce((sum,currentItem)=>sum+currentItem);
+
+//Дан массив из чисел.Найдите сумму этих чисел.
+const sumItems = testArray.reduce((sum, currentItem) => sum + currentItem);
 console.log(sumItems);
-//6
+
+//Дан массив, в нем могут быть обычные элементы и подмассивы. Оставьте в нем только подмассивы.
 const secondTestArray = [1, 2, [3, 4], 5, {a: 7}];
 const subArray = secondTestArray.filter(item => Array.isArray(item));
 console.log(subArray);
-//7
+
+//Верните количество слов в строке, длина которых больше 4 символов.
 const testString = 'asd rffvg asef asdasfasdf as';
 const arrOfString = testString.split(' ');
 const resultString = arrOfString.filter(item => item.length > 4 );
 console.log(resultString.length);
-//8
+
+//Если в строке имеется шестизначное число(ни больше, ни меньше) - верните его, как результат функции. Число может быть не отделено от других символов пробелами.
 const secondTestString = 'asfsad123123456asd 888888 123 10 21 6543221534';
 console.log(secondTestString.match(/(?<!\d)\d{6}(?!\d)/gmi)); 
